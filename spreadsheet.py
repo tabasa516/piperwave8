198b363127f35d637d6f270fc80e5c6f455c00bf
tabasa516 <150096977+tabasa516@users.noreply.github.com>
2024-01-11 11:47:06 +0900

    Update spreadsheet.py

--- spreadsheet.py
+++ spreadsheet.py
@@ -2,18 +2,18 @@
 
 import os
 import time
+from datetime import datetime
 import pickle
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-SPREADSHEET_ID = '16rgnCIIEmjWwqkg_9WjHKy-hde8Atj_pVLx9f6Y-iJs'  # ここにGoogle SheetsのスプレッドシートのIDを入力
-SERVICE_ACCOUNT_FILE = 'elemental-axle-404504-ac76119fad9c'  # サービスアカウントのJSONファイルへのパス
+SPREADSHEET_ID = '16rgnCIIEmjWwqkg_9WjHKy-hde8Atj_pVLx9f6Y-iJs'
+SERVICE_ACCOUNT_FILE = 'elemental-axle-404504-ac76119fad9c.json'
 
 def authenticate_google_sheets():
     creds = None
-    # サービスアカウントを使用した認証
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
@@ -31,7 +31,13 @@ def write_to_google_sheets(temperature_data):
         service = build('sheets', 'v4', credentials=creds)
         sheet = service.spreadsheets()
 
-        values = [temperature_data]
+        # 現在の時刻を取得
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        # 温度データと時刻を結合
+        temperature_data_with_timestamp = [timestamp] + temperature_data
+
+        values = [temperature_data_with_timestamp]
 
         body = {'values': values}
         result = sheet.values().append(
@@ -43,8 +49,7 @@ def write_to_google_sheets(temperature_data):
         print('Data written to Google Sheets:', result)
     except Exception as e:
         print('Error writing to Google Sheets:', e)
-    
-# 以下、readSensor関数を変更して、温度データをGoogle Sheetsに書き込むようにします
+
 def readSensor(id):
     tfile = open("/sys/bus/w1/devices/"+id+"/w1_slave")
     text = tfile.read()
